a172b739a6777746d14e3fe18e755da27022adf2
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-09-21 19:19:25 +0300

    chore: add global styles and font assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./vendor/normalize.css";
+import "./vendor/fonts.css";
 import "./index.css";
 import App from "../src/components/App/App";
 
