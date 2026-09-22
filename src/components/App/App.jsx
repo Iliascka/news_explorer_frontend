c3f08c1c442c087625bc556c9e7977a8587d0adc
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-09-22 17:55:27 +0300

    feat: create header and navigation components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-
-import "./App.css";
+import Header from "../Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Hello</h1>;
+  return <Header />;
 }
 
 export default App;
